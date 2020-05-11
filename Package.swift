dfc2f53ecbac545551c631ce078e49597a424446
Aldrin Lenny <aldrin@isletsystems.com>
2020-05-12 11:07:09 +1200

    * better use lint soon.
    fixed missing coma

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "ZipArchive", 
-            path: "SSZipArchive"
+            path: "SSZipArchive",
             targets: ["ZipArchive-tvos", "ZipArchive-iOS", "ZipArchive-Mac", "ZipArchive-watchos"])
         ],
         dependencies: [
